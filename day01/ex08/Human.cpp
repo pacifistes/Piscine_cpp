256d411d6a29387e61fb2799543745b28c32a172
Benoit BRUNELLE <bbrunell@e1r10p2.42.fr>
2018-01-06 13:14:54 +0100

    ee

--- day01/ex08/Human.cpp
+++ day01/ex08/Human.cpp
@@ -1,15 +1,5 @@
 #include "Human.hpp"
 
-HumanA::HumanA(std::string name, Weapon &club) : _name(name), _club(club) {
-	std::cout << "HumanA constructor called" << std::endl;
-	return;
-}
-
-HumanA::~HumanA (void) {
-	std::cout << "HumanA destructeur called" << std::endl;
-	return;
-}
-
 void Human::meleeAttack(std::string const & target) {
 	std::cout << "Human attacks " << target;
 	std::cout << " with a melee attack" << std::endl;
@@ -26,5 +16,5 @@ void Human::intimidatingShout(std::string const &target) {
 }
 
 void Human::action(std::string const &action_name, std::string const &target) {
-
+	
 }
